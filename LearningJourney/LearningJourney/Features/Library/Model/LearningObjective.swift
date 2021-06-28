bc53172630e377d43b9410d53f0a6063077736f4
Bruno Pastre <pastre68@gmail.com>
2021-06-28 12:17:37 -0300

    Removes dumb init

--- LearningJourney/LearningJourney/Features/Library/Model/LearningObjective.swift
+++ LearningJourney/LearningJourney/Features/Library/Model/LearningObjective.swift
@@ -5,12 +5,4 @@ struct LearningObjective: Decodable, Identifiable, Equatable {
     let isCore: Bool
     let Description: String
     let isLearned: Bool
-    
-    internal init(id: String, isCore: Bool, Description: String, isLearned: Bool) {
-        self.id = id
-        self.isCore = isCore
-        self.Description = Description
-        self.isLearned = isLearned
-    }
-    
 }
