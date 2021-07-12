f3ed9474c27ef0e27c692a81c74473e98ba69543
Bruno Pastre <pastre68@gmail.com>
2021-07-12 10:59:34 -0300

    Creates JAuthentication

---
+++ LearningJourney/Features/JAuthentication/JAuthentication.h
@@ -0,0 +1,18 @@
+//
+//  JAuthentication.h
+//  JAuthentication
+//
+//  Created by Bruno Pastre on 12/07/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for JAuthentication.
+FOUNDATION_EXPORT double JAuthenticationVersionNumber;
+
+//! Project version string for JAuthentication.
+FOUNDATION_EXPORT const unsigned char JAuthenticationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JAuthentication/PublicHeader.h>
+
+
