99cc4e8297cde69c15ea6e60f80c1d0cfc47635a
Bruno Pastre <pastre68@gmail.com>
2021-07-13 09:45:29 -0300

    Creates dependency injector module

---
+++ LearningJourney/Core/CoreInjector/Sources/Dependency.swift
@@ -0,0 +1,41 @@
+protocol Resolvable {}
+
+public enum DependencyInjectionError<T>: Error {
+    case unresolvedYet(_ forType: T.Type)
+    case resolvedTwice(_ forType: T.Type)
+    case notRegistered(_ forType: T.Type)
+}
+
+@propertyWrapper
+public final class Dependency<T>: Resolvable {
+    
+    public typealias ErrorHandler = (DependencyInjectionError<T>) -> Never
+
+    private let errorHandler: ErrorHandler
+    private var resolvedValue: T?
+    
+    public var wrappedValue: T {
+        guard let resolved = resolvedValue else {
+            errorHandler(.unresolvedYet(T.self))
+        }
+        return resolved
+    }
+    
+    public init(
+        resolvedValue: T? = nil,
+        errorHandler: @escaping ErrorHandler = { preconditionFailure($0.localizedDescription) }
+    ) {
+        self.resolvedValue = resolvedValue
+        self.errorHandler = errorHandler
+    }
+    
+    func resolve(using container: DependencyContainer) {
+        guard resolvedValue == nil else {
+            errorHandler(.resolvedTwice(T.self))
+        }
+        guard let value = container.make(T.self) else {
+            errorHandler(.notRegistered(T.self))
+        }
+        resolvedValue = value
+    }
+}
