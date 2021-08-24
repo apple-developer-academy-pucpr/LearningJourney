a61e3ce78735d4575983dbf08a2b82ee88cc0454
Bruno Pastre <pastre68@gmail.com>
2021-08-24 10:08:45 -0300

    Cleans project before running tests

--- tools/Scripts/run_tests.sh
+++ tools/Scripts/run_tests.sh
@@ -1,3 +1,3 @@
 cd "$(git rev-parse --show-toplevel)"
-make generate
+make clean generate
 xcodebuild clean build test -project LearningJourney/App/LearningJourney.xcodeproj -scheme "LearningJourney - Debug" -sdk iphonesimulator -destination "platform=iOS Simulator,name=iPhone 8" CODE_SIGNING_REQUIRED=NO
