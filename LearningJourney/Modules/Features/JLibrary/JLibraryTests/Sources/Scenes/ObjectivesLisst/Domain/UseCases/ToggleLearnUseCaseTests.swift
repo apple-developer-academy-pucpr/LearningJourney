518429685a118d6f43a4169721b59cca69d17225
Vinícius Couto Tasso <vcoutasso@gmail.com>
2021-10-05 18:40:04 -0300

    Creates ToggleLearnUseCaseTests (#12)
    Co-authored-by: Bruno Pastre <6251198+pastre@users.noreply.github.com>

---
+++ LearningJourney/Modules/Features/JLibrary/JLibraryTests/Sources/Scenes/ObjectivesLisst/Domain/UseCases/ToggleLearnUseCaseTests.swift
@@ -0,0 +1,51 @@
+
+import XCTest
+@testable import JLibrary
+
+final class ToggleLearnUseCaseTests: XCTestCase {
+    // MARK: - Properties
+    
+    private let libraryRepositorySpy = LibraryRepositorySpy()
+    private lazy var sut = ToggleLearnUseCase(repository: libraryRepositorySpy)
+
+    // MARK: - Unit tests
+    
+    func test_execute_itShouldFlipLearnedFlag() {
+        // Given
+        let flagStub: Bool = .random()
+        let expectedFlag = !flagStub
+
+        let completionExpectation = expectation(description: "Completion should be called")
+        
+        // When
+        
+        sut.execute(objective: .fixture(isComplete: flagStub)) { _ in
+            completionExpectation.fulfill()
+        }
+
+        // Then
+        
+        waitForExpectations(timeout: 1, handler: nil)
+        XCTAssertEqual(libraryRepositorySpy.updateObjectiveCallCount, 1)
+        XCTAssertEqual(expectedFlag, libraryRepositorySpy.updateObjectiveNewObjectivePassed?.isComplete)
+    }
+}
+
+// MARK: - Testing doubles
+final class LibraryRepositorySpy: LibraryRepositoryProtocol  {
+    func fetchStrands(completion: @escaping Completion<[LearningStrand]>) {
+        fatalError("nyi")
+    }
+
+    func fetchObjectives(using goal: LearningGoal, completion: @escaping Completion<[LearningObjective]>) {
+        fatalError("nyi")
+    }
+
+    private(set) var updateObjectiveCallCount = 0
+    private(set) var updateObjectiveNewObjectivePassed: LearningObjective?
+    func updateObjective(newObjective: LearningObjective, completion: @escaping Completion<LearningObjective>) {
+        updateObjectiveCallCount += 1
+        updateObjectiveNewObjectivePassed = newObjective
+        completion(.failure(.unknown))
+    }
+}
