252ad9bb84fa1a9347e0880274e98ec917db0d1f
pastre <pastre68@gmail.com>
2022-04-07 21:47:31 -0300

    Updates testing script

--- tools/Scripts/run_tests.sh
+++ tools/Scripts/run_tests.sh
@@ -1,3 +1,8 @@
 cd "$(git rev-parse --show-toplevel)"
 make clean generate # TODO this sjhould be another CI step
-xcodebuild clean build test -workspace LearningJourney/LearningJourney.xcworkspace -scheme "LearningJourney - Debug" -sdk iphonesimulator -destination "platform=iOS Simulator,name=iPhone 8" CODE_SIGNING_REQUIRED=NO
+xcodebuild clean build test \
+    -project LearningJourney/App/LearningJourney.xcodeproj \
+    -scheme "LearningJourney - Debug" \
+    -sdk iphonesimulator \
+    -destination "platform=iOS Simulator,name=iPhone 8" \
+    CODE_SIGNING_REQUIRED=NO
