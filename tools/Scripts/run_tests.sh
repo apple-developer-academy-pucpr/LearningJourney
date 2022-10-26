55b51bd5c7de4056544c834982904ac769b06632
Vinícius Couto Tasso <vcoutasso@gmail.com>
2022-10-26 14:21:03 -0300

    create a dedicate CI step to generate project files

--- tools/Scripts/run_tests.sh
+++ tools/Scripts/run_tests.sh
@@ -1,5 +1,5 @@
 cd "$(git rev-parse --show-toplevel)"
-make clean generate # TODO this sjhould be another CI step
+
 xcodebuild clean build test \
     -project LearningJourney/App/LearningJourney.xcodeproj \
     -scheme "LearningJourney - Debug" \
